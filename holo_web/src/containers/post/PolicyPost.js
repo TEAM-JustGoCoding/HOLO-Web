be7b12cc9e5f97221284c9b770e633c5d20ce6c3
sheoae12 <58352301+sheoae12@users.noreply.github.com>
2022-05-09 13:07:09 +0900

    Update PolicyPost.js

--- holo_web/src/containers/post/PolicyPost.js
+++ holo_web/src/containers/post/PolicyPost.js
@@ -3,22 +3,68 @@ import React, {useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {images} from '../../images';
 import { AiOutlineEye, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import axios from 'axios';
 
-const user = "우네"
-const title = "구미시 정책"
-const content = "구미시 정책을 공유합니다"
-const reg_date = "2022-04-13 21:23:17"
-const view = "5"
-const like = "5"
+var user = "구리"
+var title = "구미시 정책"
+var content = "구미시 정책을 공유합니다"
+var reg_date = "2022-04-13 21:23:17"
+var view = "5"
+var like = "5"
+
+function getPost() {
+  /*
+  axios.get('http://holo.dothome.co.kr/policyPost.json', {
+      withCredentials: false,
+      headers: {"Content-Type": "application/json"}
+    })
+    .then((response) => {
+      console.log("읽어옴");
+      console.log(response);
+     });
+     */
+     return fetch('https://stark-savannah-03205.herokuapp.com/http://holo.dothome.co.kr/poliPost.json')
+     .then(response => { return response.json();})
+     .then(response => { 
+                         var obj = response;
+                         console.log(obj);
+   
+                         user = obj[0].nick_name;
+                         title = obj[0].title;
+                         content = obj[0].content;
+                         reg_date = obj[0].reg_date;
+                         view = obj[0].view;
+                         like = obj[0].like;
+                         
+                       });
+}
+
+function requestPost(id){
+  axios.post("http://holo.dothome.co.kr/findPolicyPost.php", JSON.stringify({postid: id}),{
+      withCredentials: false,
+      headers: {"Content-Type": "application/json"}
+    })
+      .then(function(body) {
+        console.log(body);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+}
 
 function Post() {
   const {id} = useParams();
   const [heart, setHeart] = useState(false);
 
+  //console.log(id);
+  requestPost(id);
+
+  getPost();
+
   return (
     <div>
       <div className="postHeaderBar">
-        <div>정책</div>
+        <div>생활백서</div>
       </div>
       <div className="postTitle">{title}</div>
       <div className="postUser"><img src={images.user} alt="User"/>{user}</div>
@@ -28,8 +74,8 @@ function Post() {
         <div className="postEtc">
             <AiOutlineEye style={{ fontSize: '3.5vh', marginRight: '1vh'}}/>{view}
             {heart
-              ? <AiFillHeart className="heartIcon red" onClick={() => { setHeart(false);} }/>
-              : <AiOutlineHeart className="heartIcon" onClick={() => { setHeart(true);} }/>
+              ? <AiFillHeart className="heartIcon red" onClick={() => { setHeart(false);}}/>
+              : <AiOutlineHeart className="heartIcon" onClick={() => { setHeart(true);}}/>
             }
             {like}
         </div>
