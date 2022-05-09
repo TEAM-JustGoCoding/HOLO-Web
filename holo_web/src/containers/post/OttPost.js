5df782b33fb61aae38d5fa7b7cfde19a888438ec
sheoae12 <58352301+sheoae12@users.noreply.github.com>
2022-05-09 21:39:37 +0900

    Update OttPost.js

--- holo_web/src/containers/post/OttPost.js
+++ holo_web/src/containers/post/OttPost.js
@@ -5,22 +5,59 @@ import Modal from '../../components/Modal';
 import {images} from '../../images';
 import { AiOutlineEye } from "react-icons/ai";
 import { FaRegLaugh, FaRegLaughSquint } from "react-icons/fa";
+import axios from 'axios';
 
-const user = "김태리짱"
-const title = "넷플릭스 구독하실분!"
-const content = "이번달부터 넷플릭스 같이 구독하실 분 계신가요?\n네 명 모여서 같이 이용해요!\n스물다섯스물하나 보고 싶네요ㅠㅠ"
-const reg_date = "2022-04-06 08:23:17"
-const limit_date = "2022-04-10"
-const buy_location = "넷플릭스"
-const goal = "4"
-const accumulate = "3"
-const view = "3"
+var user = "김태리짱"
+var title = "넷플릭스 구독하실분!"
+var content = "이번달부터 넷플릭스 같이 구독하실 분 계신가요?\n네 명 모여서 같이 이용해요!\n스물다섯스물하나 보고 싶네요ㅠㅠ"
+var reg_date = "2022-04-06 08:23:17"
+var limit_date = "2022-04-10"
+var buy_location = "넷플릭스"
+var goal = "4"
+var accumulate = "3"
+var view = "3"
+
+function getPost() {
+  return fetch('https://stark-savannah-03205.herokuapp.com/http://holo.dothome.co.kr/OTTPost.json')
+  .then(response => { return response.json();})
+	.then(response => { 
+                      var obj = response;
+                      console.log(obj);
+
+                      user = obj[0].nick_name;
+                      title = obj[0].title;
+                      content = obj[0].content;
+                      reg_date = obj[0].reg_date;
+                      limit_date = obj[0].limit_date;
+                      goal = obj[0].goal;
+                      accumulate = obj[0].accumulate;
+                      view = obj[0].view;
+                      buy_location = obj[0].buy_location;
+                    });
+}
+
+function requestPost(id){
+  axios.post("http://holo.dothome.co.kr/findOTTPost.php", JSON.stringify({postid: id}),{
+      withCredentials: false,
+      headers: {"Content-Type": "application/json"}
+    })
+      .then(function(body) {
+        console.log(body);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+}
+
 
 function Post() {
   const {id} = useParams();
   const [participation, setParticipation] = useState(false);
   const [checkModalOpen, setCheckModalOpen] = useState(false);
   const [infoModalOpen, setInfoModalOpen] = useState(false);
+
+  requestPost(id);
+  getPost();
 
   const msg = "\nOTT 구독자 모집에 참여하시겠습니까?\n추후 취소는 불가능합니다.\n신중하게 결정해주세요!"
   const submitModal = () => {
